8192c44e322fc37c157bd2f9d7ab55907b1807d9
Grace Choi <choi.g.330@gmail.com>
2021-05-06 22:44:34 -0700

    landscape floor created

--- creepcraft/game.py
+++ creepcraft/game.py
@@ -1,98 +1,163 @@
-# main game logic (move / jump / place blocks / tally score points)
+from pyglet import image, app
+from pyglet.gl import *
+from pyglet.window import key, mouse 
+import math
 
-# other, create cubes, get textures, some used by both window class and model
+class Model:
 
+    def get_tex(self,file):
+        tex = pyglet.image.load(file).get_texture()
+        # fixes the pixelation of the block face (makes less blurry - min&max filter)
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+        return pyglet.graphics.TextureGroup(tex)
 
-# When we get a file of textures, these coords point to the spot in the picture we are pulling from. 
-# TEXTURE_PATH = '.png of textures here'
-# GRASS = texture_position((1, 0), (0,1), (0, 0))
-# STONE = texture_position((1, 0), (0,1), (0, 0))
-# DIRT = texture_position((1, 0), (0,1), (0, 0))
+    def add_block(self,x,y,z):
+        # defines four 3D vertices of a cube
+        # this position is right where the camera is facing -> does not show blocks when perspective is set (gluPerspective)
+        # x,y,z = 0,0,0
+        # get adjacent cubes (X, Y, Z)
+        X, Y, Z = x+1, y+1, z+1
 
-from window import Window
+        # texture mapping (2 floats - 2D): http://www.glprogramming.com/red/chapter09.html
+        # 2D tex coordinates: range from 0.0-1.0 in both directions
+        # square tex coordinates: (0, 0), (1, 0), (1, 1), and (0, 1)
+        tex_coords = ('t2f', (0,0, 1,0, 1,1, 0,1))
 
-PLAY_SPACE_SIZE = 16
-FACES = [
-    ( 0, 1, 0),
-    ( 0,-1, 0),
-    (-1, 0, 0),
-    ( 1, 0, 0),
-    ( 0, 0, 1),
-    ( 0, 0,-1),
-]
+        # one single block:
+        # back & front
+        self.batch.add(4, GL_QUADS, self.side,   ('v3f', (X,y,z,  x,y,z,  x,Y,z,  X,Y,z)), tex_coords) 
+        self.batch.add(4, GL_QUADS, self.side,   ('v3f', (x,y,Z,  X,y,Z,  X,Y,Z,  x,Y,Z)), tex_coords) 
 
-def cube_position(x, y, z, n):
-    """[return verticies of cube, n references window size. Each row is a side of the cube, with three verticies pointing to the corners of that side.]
+        # left & right
+        self.batch.add(4, GL_QUADS, self.side,   ('v3f', (x,y,z,  x,y,Z,  x,Y,Z,  x,Y,z)), tex_coords) 
+        self.batch.add(4, GL_QUADS, self.side,   ('v3f', (X,y,Z,  X,y,z,  X,Y,z,  X,Y,Z)), tex_coords) 
 
-    Args:
-        x ([int]): [description]
-        y ([int]): [description]
-        z ([int]): [description]
-        n ([int]): [description]
-    """
-    return [
-        x-n,y+n,z-n, x-n,y+n,z+n, x+n,y+n,z+n, x+n,y+n,z-n,  # top
-        x-n,y-n,z-n, x+n,y-n,z-n, x+n,y-n,z+n, x-n,y-n,z+n,  # bottom
-        x-n,y-n,z-n, x-n,y-n,z+n, x-n,y+n,z+n, x-n,y+n,z-n,  # left
-        x+n,y-n,z+n, x+n,y-n,z-n, x+n,y+n,z-n, x+n,y+n,z+n,  # right
-        x-n,y-n,z+n, x+n,y-n,z+n, x+n,y+n,z+n, x-n,y+n,z+n,  # front
-        x+n,y-n,z-n, x-n,y-n,z-n, x-n,y+n,z-n, x+n,y+n,z-n,  # back
-    ]
+        # bottom & right
+        self.batch.add(4, GL_QUADS, self.bottom, ('v3f', (x,y,z,  X,y,z,  X,y,Z,  x,y,Z)), tex_coords)  
+        self.batch.add(4, GL_QUADS, self.top, ('v3f', (x,Y,Z,  X,Y,Z,  X,Y,z,  x,Y,z)), tex_coords)  
 
-def texture_position(x, y, n=4):
-    """[return verticies of texture square]"""
-    pass
+    def world(self):
+        for x in range(50):
+            for y in range(50):
+                self.add_block(x, 0, y)
 
-def texture_list(top, bottom, side):
-    """[return a list of the texture squares]
+    def __init__(self):
+        # faces of a single block
+        self.top = self.get_tex('textures/grass_top.png')
+        self.side = self.get_tex('textures/grass_side.png')
+        self.bottom = self.get_tex('textures/dirt.png')
 
-    Args:
-        top ([type]): [description]
-        bottom ([type]): [description]
-        side ([type]): [description]
-    """
-    pass
+        self.batch = pyglet.graphics.Batch()
+        self.world()
 
-def normalize(position):
-    """[takes in the position, rounds each verticie, and returns block in that position]
+    def draw(self):
+        self.batch.draw()
 
-    Args:
-        position ([int]): [tuple length of three]
-    """
-    pass
+class Player:
+    def __init__(self):
+        # position - 0,0,0 coordinates don't show the world in the frame without movement 
+        # shift coords so that players actually sees the world upon opening the window
+        # self.pos = [0,0,0]
+        # self.rot = [0,0]
+        self.pos = [1,2,2]
+        self.rot = [-15,0]
 
-def play_space(position):
-    """[returns tuple the given position in in]
+    def update(self,dt,keys):
+        """Actually updates the window based on 'dt' (ticks per second)"""
+        slower = 0.2
+        s = dt*10
+        rotY = -self.rot[1]/180*math.pi
+        # dx,dy are used to change the position of the player within the world 
+        # key handler - if key pressed is True, change position
+        dx, dz = s*math.sin(rotY), math.cos(rotY)
+        # pos[0]: x, pos[2]: z
+        if keys[key.W]: # walk forward
+            self.pos[0] += dx*slower
+            self.pos[2] -= dz*slower
+        if keys[key.S]: # walk backwards
+            self.pos[0] -= dx*slower
+            self.pos[2] += dz*slower
+        if keys[key.A]: # left
+            self.pos[0] -= dz*slower
+            self.pos[2] -= dx*slower
+        if keys[key.D]: # right
+            self.pos[0] += dz*slower
+            self.pos[2] += dx*slower
+        # pos[1]: y (up/down)
+        if keys[key.SPACE]: # up
+            self.pos[1] += s
+        if keys[key.LSHIFT]: # down
+            self.pos[1] -= s
 
-    Args:
-        position ([position]): [tuple length of three]
-        returns ([sector]): [tuple length of three]
-    """
-    pass
+class Window(pyglet.window.Window):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        #logo
+        self.logo_image = pyglet.image.load('logo.png')
+        self.logo = pyglet.sprite.Sprite(self.logo_image, x=70, y=650)
+        # min size of window so it doesn't error out if user tries to make it too small
+        self.set_minimum_size(400,300)
+        # key state handler holds a bool value - https://pythonhosted.org/pyglet/api/pyglet.window.key.KeyStateHandler-class.html
+        self.keys = key.KeyStateHandler()
+        self.push_handlers(self.keys)
+        # need scheduler for update to work
+        # key press based on time of holding down key
+        pyglet.clock.schedule(self.update)
+        self.model = Model()
+        self.player = Player()
 
-def set_up():
-    """[Basic OpenGL configuration to run game]
-    """
-    # set color of the sky
+    # define the perspective (set between 2d and 3d) - in order to see the world
+    def Projection(self):
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+
+    def Model(self):
+        glMatrixMode(GL_MODELVIEW)
+        glLoadIdentity()
+
+    def set2d(self):
+        self.Projection()
+        gluPerspective(0, self.width, 0, self.height)
+        self.Model()
+
+    def set3d(self):
+        self.Projection()
+        gluPerspective(70, self.width/self.height, 0.05, 1000)
+        self.Model()
+
+    def on_key_press(self, KEY, _MOD):
+        if KEY == key.ESCAPE:
+            self.close()
+        elif KEY == key.E:
+            self.mouse_lock = not self.mouse_lock
+
+    def update(self, dt):
+        # updates key events from Player class (changing player position with movement controls)
+        self.player.update(dt, self.keys)
+
+    def on_draw(self):
+        self.clear()
+        self.set3d()
+        self.logo.draw()
+        glColor3d(1, 1, 1)
+        # rotation (skews the depth of the block)
+        # glRotatef(-30,1,0,0)
+        x,y,z = self.player.pos
+        # move scene around to show individual cube/block
+        # glTranslatef(0,0,-2)
+        glTranslatef(-x,-y,-z)
+        self.model.draw()
+
+def setup():
+    # # color codes: https://pemavirtualhub.wordpress.com/2016/06/20/opengl-color-codes/
     glClearColor(0.5, 0.69, 1.0, 1)
-    # stop render of cubes the user can't see
-    glEnable(GL_CULL_FACE)
-    # GL_NEAREST
-    # "is generally faster than GL_LINEAR, but it can produce textured images
-    # with sharper edges because the transition between texture elements is not
-    # as smooth."
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
-    # call fog() here if we write this!
+    glEnable(GL_DEPTH_TEST)
 
 def main():
-    window = Window(width=800, height=600, caption='Pyglet', resizable=True)
-    # Hide the mouse cursor and it can't leave window.
-    window.set_exclusive_mouse(True)
-    set_up()
-    # pyglet.app.run()
-    app.run()
-
+    window = Window(width=1000, height=800, caption='CREEPCRAFT',resizable=True)
+    setup()
+    pyglet.app.run()
 
 if __name__ == '__main__':
     main()
