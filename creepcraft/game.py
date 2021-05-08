987a2594f30be07f6a3848728c39808d637520c6
Grace Choi <choi.g.330@gmail.com>
2021-05-08 15:58:25 -0700

    add blocks working

--- creepcraft/game.py
+++ creepcraft/game.py
@@ -4,6 +4,16 @@ from pyglet.window import key, mouse
 import math
 
 class Model:
+    def __init__(self):
+        # faces of block
+        self.grass_top = self.get_tex('textures/grass_top.png')
+        self.grass_side = self.get_tex('textures/grass_side.png')
+        self.grass_bottom = self.get_tex('textures/dirt.jpeg')
+        self.stone = self.get_tex('textures/stone.png')
+        self.batch = pyglet.graphics.Batch()
+        self.current_world = {}
+        self.player = Player()
+        self.starter_world()
 
     def get_tex(self,file):
         tex = pyglet.image.load(file).get_texture()
@@ -12,7 +22,11 @@ class Model:
         glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
         return pyglet.graphics.TextureGroup(tex)
 
-    def add_block(self,x,y,z):
+    def add_block(self, x, y, z, texture):
+        # self.current_world[(x,y,z)] = self.player.pos
+
+        # need to do: add texture parameter to specify which type of block
+
         # defines four 3D vertices of a cube
         # this position is right where the camera is facing -> does not show blocks when perspective is set (gluPerspective)
         # x,y,z = 0,0,0
@@ -26,30 +40,34 @@ class Model:
 
         # one single block:
         # back & front
-        self.batch.add(4, GL_QUADS, self.side,   ('v3f', (X,y,z,  x,y,z,  x,Y,z,  X,Y,z)), tex_coords) 
-        self.batch.add(4, GL_QUADS, self.side,   ('v3f', (x,y,Z,  X,y,Z,  X,Y,Z,  x,Y,Z)), tex_coords) 
+        self.batch.add(4, GL_QUADS, texture[0], ('v3f', (X,y,z,  x,y,z,  x,Y,z,  X,Y,z)), tex_coords) 
+        self.batch.add(4, GL_QUADS, texture[0], ('v3f', (x,y,Z,  X,y,Z,  X,Y,Z,  x,Y,Z)), tex_coords) 
 
         # left & right
-        self.batch.add(4, GL_QUADS, self.side,   ('v3f', (x,y,z,  x,y,Z,  x,Y,Z,  x,Y,z)), tex_coords) 
-        self.batch.add(4, GL_QUADS, self.side,   ('v3f', (X,y,Z,  X,y,z,  X,Y,z,  X,Y,Z)), tex_coords) 
+        self.batch.add(4, GL_QUADS, texture[0], ('v3f', (x,y,z,  x,y,Z,  x,Y,Z,  x,Y,z)), tex_coords) 
+        self.batch.add(4, GL_QUADS, texture[0], ('v3f', (X,y,Z,  X,y,z,  X,Y,z,  X,Y,Z)), tex_coords) 
 
         # bottom & right
-        self.batch.add(4, GL_QUADS, self.bottom, ('v3f', (x,y,z,  X,y,z,  X,y,Z,  x,y,Z)), tex_coords)  
-        self.batch.add(4, GL_QUADS, self.top, ('v3f', (x,Y,Z,  X,Y,Z,  X,Y,z,  x,Y,z)), tex_coords)  
+        self.batch.add(4, GL_QUADS, texture[2], ('v3f', (x,y,z,  X,y,z,  X,y,Z,  x,y,Z)), tex_coords)  
+        self.batch.add(4, GL_QUADS, texture[1], ('v3f', (x,Y,Z,  X,Y,Z,  X,Y,z,  x,Y,z)), tex_coords)  
 
-    def world(self):
+    def remove_block(self, x, y, z):
+        # del self.current_world[(x,y-1,z-3)]
+        pass
+
+    def starter_world(self):
+        GRASS = [self.grass_side, self.grass_top, self.grass_bottom]
+        STONE = [self.stone, self.stone, self.stone]
+        y = 0
+        # floor
         for x in range(50):
-            for y in range(50):
-                self.add_block(x, 0, y)
-
-    def __init__(self):
-        # faces of a single block
-        self.top = self.get_tex('textures/grass_top.png')
-        self.side = self.get_tex('textures/grass_side.png')
-        self.bottom = self.get_tex('textures/dirt.png')
-
-        self.batch = pyglet.graphics.Batch()
-        self.world()
+            for z in range(50):
+                self.add_block(x, y, z, GRASS)
+                if x in range(1) or z in range(1):
+                    for Y in range(0, 15):
+                        self.add_block(x, Y, z, STONE)
+                        self.add_block(49, Y, z, STONE)
+                        self.add_block(x, Y, 49, STONE)
 
     def draw(self):
         self.batch.draw()
@@ -60,42 +78,50 @@ class Player:
         # shift coords so that players actually sees the world upon opening the window
         # self.pos = [0,0,0]
         # self.rot = [0,0]
-        self.pos = [1,2,2]
+        self.pos = [30,2,30]
         self.rot = [-15,0]
 
     def update(self,dt,keys):
         """Actually updates the window based on 'dt' (ticks per second)"""
-        slower = 0.2
+        slower = .5
         s = dt*10
         rotY = -self.rot[1]/180*math.pi
-        # dx,dy are used to change the position of the player within the world 
+        # dx,dy are the velocity used to change the position of the player within the world 
         # key handler - if key pressed is True, change position
-        dx, dz = s*math.sin(rotY), math.cos(rotY)
-        # pos[0]: x, pos[2]: z
+        dx, dz = s*math.sin(rotY), s*math.cos(rotY)
+        # pos[0]: x (left/right)
+        # pos[2]: z (front/back)
         if keys[key.W]: # walk forward
-            self.pos[0] += dx*slower
-            self.pos[2] -= dz*slower
+            # 2 -> front limit
+            if self.pos[2] >= 2:
+                self.pos[0] += dx*slower
+                self.pos[2] -= dz*slower
         if keys[key.S]: # walk backwards
-            self.pos[0] -= dx*slower
-            self.pos[2] += dz*slower
+            if self.pos[2] <= 49:
+                self.pos[0] -= dx*slower
+                self.pos[2] += dz*slower
         if keys[key.A]: # left
-            self.pos[0] -= dz*slower
-            self.pos[2] -= dx*slower
+            if self.pos[0] >= 2:
+                self.pos[0] -= dz*slower
+                self.pos[2] -= dx*slower
         if keys[key.D]: # right
-            self.pos[0] += dz*slower
-            self.pos[2] += dx*slower
+            if self.pos[0] <= 48:
+                self.pos[0] += dz*slower
+                self.pos[2] += dx*slower
         # pos[1]: y (up/down)
         if keys[key.SPACE]: # up
-            self.pos[1] += s
+            # 6 -> jump limit
+            if self.pos[1] <= 6:
+                self.pos[1] += s
         if keys[key.LSHIFT]: # down
-            self.pos[1] -= s
+            # 2 -> floor limit
+            if self.pos[1] >= 2:
+                self.pos[1] -= s
 
 class Window(pyglet.window.Window):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        #logo
-        self.logo_image = pyglet.image.load('logo.png')
-        self.logo = pyglet.sprite.Sprite(self.logo_image, x=70, y=650)
+        self.logo = pyglet.image.load('logo.png')
         # min size of window so it doesn't error out if user tries to make it too small
         self.set_minimum_size(400,300)
         # key state handler holds a bool value - https://pythonhosted.org/pyglet/api/pyglet.window.key.KeyStateHandler-class.html
@@ -126,20 +152,39 @@ class Window(pyglet.window.Window):
         gluPerspective(70, self.width/self.height, 0.05, 1000)
         self.Model()
 
-    def on_key_press(self, KEY, _MOD):
-        if KEY == key.ESCAPE:
-            self.close()
-        elif KEY == key.E:
-            self.mouse_lock = not self.mouse_lock
-
     def update(self, dt):
         # updates key events from Player class (changing player position with movement controls)
         self.player.update(dt, self.keys)
 
+    def on_key_press(self, symbol, modifiers):
+        if symbol == key.ESCAPE:
+            self.close()
+
+    def on_mouse_motion(self, x, y, dx, dy):
+        # stretch goal
+        # create method if we get to the point of adding rotation perspective
+        pass
+
+    def on_mouse_press(self, x, y, button, modifiers):
+        # https://pyglet.readthedocs.io/en/latest/programming_guide/mouse.html 
+        # The x and y parameters give the coordinates of the mouse pointer, relative to the bottom-left corner of the window.
+        x,y,z = self.player.pos 
+        GRASS = [self.model.grass_side, self.model.grass_top, self.model.grass_bottom]
+        STONE = [self.model.stone, self.model.stone, self.model.stone]
+        if button == mouse.RIGHT:
+            print("right button clicked")
+            self.model.remove_block(x,y,z)
+            # print("block deleted")
+        elif button == mouse.LEFT:
+            print("block created")
+            self.model.add_block(x,y-1,z-3,GRASS)
+
+    def collision(self, position):
+        pass
+
     def on_draw(self):
         self.clear()
         self.set3d()
-        self.logo.draw()
         glColor3d(1, 1, 1)
         # rotation (skews the depth of the block)
         # glRotatef(-30,1,0,0)
@@ -148,7 +193,12 @@ class Window(pyglet.window.Window):
         # glTranslatef(0,0,-2)
         glTranslatef(-x,-y,-z)
         self.model.draw()
-
+        # Images with an alpha channel can be blended with the existing framebuffer. To do this you need to supply OpenGL with a blend equation. 
+        # https://pyglet.readthedocs.io/en/latest/programming_guide/image.html
+        glEnable(GL_BLEND)
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+        self.logo.blit(5,6,2, width=40, height=8)
+        
 def setup():
     # # color codes: https://pemavirtualhub.wordpress.com/2016/06/20/opengl-color-codes/
     glClearColor(0.5, 0.69, 1.0, 1)
@@ -158,6 +208,8 @@ def main():
     window = Window(width=1000, height=800, caption='CREEPCRAFT',resizable=True)
     setup()
     pyglet.app.run()
+    # get current window location
+    print(window.get_location())
 
 if __name__ == '__main__':
     main()
